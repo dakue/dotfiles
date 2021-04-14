53f4384a21dfbfaa3a668a926bdb786bbb72ccd2
Daniel Kühne <dkhmailto@gmail.com>
2021-04-14 17:33:38 +0200

    make installations conditional

--- run.sh
+++ run.sh
@@ -1,7 +1,36 @@
 #!/bin/bash
 
 # tmux tpm
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+if [ ! -d "$HOME/.tmux/plugins/tpm" ]
+then
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
 
 # vim plug
-curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+if [ ! -f "${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site/autoload/plug.vim" ]
+then
+  curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+mkdir -p ~/.config/nvim
+ln -s $(pwd)/config/nvim/init.vim $HOME/.config/nvim/init.vim
+nvim --headless +PlugInstall +UpdateRemotePlugins +qall
+
+pacman -Q nodejs npm > /dev/null
+if [ $? -eq 1 ]
+then
+  sudo pacman -Sy --noconfirm nodejs npm
+fi
+ln -s $(pwd)/.npmrc $HOME/.npmrc
+mkdir -p $HOME/.local/npm
+
+npm list -g bash-language-server yaml-language-server > /dev/null
+if [ $? -eq 1 ]
+then
+  npm install -g bash-language-server yaml-language-server
+fi
+
+pacman -Q chromium xss-lock i3lock xclip neovim python-pynvim python-pip ansible > /dev/null
+if [ $? -eq 1 ]
+then
+  sudo pacman -Sy --noconfirm chromium xss-lock i3lock xclip neovim python-pynvim python-pip ansible
+fi
